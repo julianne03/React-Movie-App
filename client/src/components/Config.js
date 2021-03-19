0441ea8a44f8983f021d6c4a2f969edfe0f1c8ae
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-20 00:44:45 +0900

    get TheMovieDB API

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'ad2b77dd0ca6d9775b63a4551731ffd7';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
