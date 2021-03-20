46cfa16c8a1f36d1a6a21d0730099a202a1d64d3
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-21 03:25:49 +0900

    Making remove, add favorte router

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -20,7 +20,6 @@ router.post('/favorited', (req, res) => {
     Favorite.find({ "movieId" : req.body.movieId, "userFrom": req.body.userFrom })
         .exec((err, info) => {
             if(err) return res.status(400).send(err)
-            
             let result = false;
             if(info.length != 0) {
                 result = true
@@ -28,6 +27,25 @@ router.post('/favorited', (req, res) => {
             // 프론트에 true or false 알려주기
             res.status(200).json({ success: true, favorited: result })
         })
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+
+    Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom: req.body.userFrom })
+        .exec((err, doc) => {
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, doc })
+        })
+})
+
+router.post('/addToFavorite', (req, res) => {
+
+    const favorite = new Favorite(req.body)
+
+    favorite.save((err, doc) => {
+        if(err) return res.status(400).send(err)
+        return res.status(200).json({ success: true })
+    })
 })
 
 module.exports = router;
