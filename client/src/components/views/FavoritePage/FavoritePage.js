3542d720011cc181a50daf007470a36da35cfeb1
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-21 13:10:21 +0900

    FavoritePage - show favorited movie poster with Popover

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,13 +1,18 @@
 import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import './favorite.css'
+import './favorite.css';
+import { Button, Popover } from 'antd';
+import { IMAGE_BASE_URL } from '../../Config';
 
 function FavoritePage() {
 
     const [Favorites, setFavorites] = useState([])
 
     useEffect(() => {
-        
+        fetchFavoritedMovie()
+    }, [])
+
+    const fetchFavoritedMovie = () => {
         Axios.post('/api/favorite/getFavoritedMovie', { userFrom: localStorage.getItem('userId') })
         .then(response => {
             if(response.data.success) {
@@ -16,7 +21,30 @@ function FavoritePage() {
                 alert('영화 정보를 가져오는데 실패 하였습니다.')
             }
         })
-    }, [])
+    }
+
+    const renderCards = Favorites.map((favorite, index) => {
+
+        const content = (
+            <div>
+                {favorite.moviePost ?
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no image"
+                }
+            </div>
+        )
+
+        return <tr key={index}>
+
+        <Popover content={content} title={`${favorite.movieTitle}`}>
+            <td>{favorite.movieTitle}</td>
+        </Popover>
+
+        <td>{favorite.movieRunTime} mins</td>
+        <td><Button>Remove</Button></td>
+
+    </tr>
+        
+    })
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
@@ -32,14 +60,7 @@ function FavoritePage() {
                     </tr>
                 </thead>
                 <tbody>
-
-                    {Favorites.map((favorite, index) => (
-                        <tr key={index}>
-                            <td>{favorite.movieTitle}</td>
-                            <td>{favorite.movieRunTime} mins</td>
-                            <td><button>Remove</button></td>
-                        </tr>
-                    ))}
+                    {renderCards}
                 </tbody>
             </table>
         </div>
